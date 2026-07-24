c238b19bc7c7380ced843438eb77d017aac82186
Emii-lia <fiaromiangaly@gmail.com>
2026-07-24 14:24:42 +0300

    test(yewi-seo): adding a basic example with a yewi project

---
+++ examples/basic/src/app/not_found/mod.rs
@@ -0,0 +1,21 @@
+use yew::{component, html, Html};
+
+#[component(NotFound)]
+pub(crate) fn not_found() -> Html {
+  html! {
+    <main class="NotFound">
+      <div class="not-found-container">
+        <div class="not-found-header">
+          <h1 class="not-found-title">
+            {"404"}
+          </h1>
+          <div class="not-found-line" />
+        </div>
+
+        <h2 class="not-found-subtitle">
+          {"Page Not Found"}
+        </h2>
+      </div>
+    </main>
+  }
+}
